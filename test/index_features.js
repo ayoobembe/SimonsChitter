e228b0581313266510be6893447666d0dd2c9c75
danielobembe <ayo.obembe@gmail.com>
2014-12-10 19:28:00 +0000

    test for button and css coded

--- test/index_features.js
+++ test/index_features.js
@@ -3,14 +3,26 @@ describe('homepage',function() {
     casper.start('http://localhost:3000');
   });
 
+  // it('should load css',function() {
+  //   expect('site.css').should.be.loaded;
+  // });
+
   it('Welcome message', function(){
     casper.then(function(){
       expect('body').to.contain.text("Welcome!");
     });
   });
+
+  // it('Button SignUp', function(){
+  //   casper.then(function(){
+  //     expect(this.click('.signup')).to.redirectTo("/signup");
+  //   });
+  // });
+
   it('Button SignUp', function(){
     casper.then(function(){
-      expect('body')
-    })
-  })
+      this.clickLabel('Sign up', 'button');
+    });
+  });
+
 });
